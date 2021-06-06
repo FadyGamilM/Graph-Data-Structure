c85c9b365abca05655a36f432a3810736bf6a814
fady gamil mahrous masoud <1700961@eng.asu.edu.eg>
2021-06-07 00:06:58 +0200

    Addedd BFS Algorithm

--- Graph.cpp
+++ Graph.cpp
@@ -1,6 +1,9 @@
 #include "iostream"
 #include "vector"
+#include "queue"
+#include "unordered_map"
 using namespace std;
+
 
 // Adjacency List Representation..
 class Graph
@@ -9,19 +12,21 @@ private:
    int Vertices;
    // define array of lists, and size isn't defined at compile time, so we will define it using this pointer syntax..
    // Note that the size of this array of lists = |V|, where |V| is number of vertices
-   vector<int>*AdjacencyList;
+   //vector<string> *AdjacencyList;
+   unordered_map<string,vector<string>> AdjacencyList;
 public:
    /****** Constructor ****************/
    Graph( int V){
       // set number of vertices..
       this->Vertices=V;
       // now number of vertices is known, so we can set the size of our array of lists
-      this->AdjacencyList=new vector<int>[V];
+      //this->AdjacencyList=new vector<string>[V];
    }
    /************************************/
 
    /**************  Add Edge Function *********************/
-   void add_Edge(int first_vertex, int second_vertex ){
+   void add_Edge(string first_vertex, string second_vertex ){
+      ////===> assume that all edges are bi-dierctional..
       // Construct/Link the edge between first and second vertix by adding the second_vertix in the AdjacencyList of first_vertix
       AdjacencyList[first_vertex].push_back(second_vertex);
       // Construct/Link the edge between second and first vertix by adding the first_vertix in the AdjacencyList of second_vertix
@@ -31,25 +36,83 @@ public:
 
    /**************  Iterate through all vertices *********************/
    void print_Ajdacency_List(){
-      for(auto i=0;i<this->Vertices;i++){
-         cout<<"Vertex "<< i << " --> ";
-         for(auto connected_vertex:this->AdjacencyList[i]){
-            cout<<connected_vertex <<", ";
-         } 
+      for(auto item:this->AdjacencyList){
+         //item : <vertexName:[Adjacency List,.....]>
+         cout<<item.first<<" --> ";
+         auto Connected_List=item.second;
+         for(auto connected_Vertex:Connected_List){
+            cout<<connected_Vertex<<", ";
+         }
          cout<<endl;
       }
    }
    /******************************************************************/
+
+   /******************* Breadth First Search (BFS) ALgorithm ******************************/
+   void BFS(string Start_Vertex){
+      // we need 2 additional data structure..
+      //==> [1] queue to store on it the connected vertices of current source.
+      //==> [2] Visited map that track if this vertex is vistied befor or not.
+      unordered_map<string,bool> seen; //[2]
+      queue<string> Container; //[1]
+      
+      /******* Step [1] : Push the given source vertex to queue ********/
+      Container.push(Start_Vertex);
+      /*******************************************************************/
+
+      /****** Loop untill queue becomes Empty ***********************/
+      while(!Container.empty()){
+         /******* Step [2] : pop the front vertex from queue ********/
+         string Node_to_be_removed=Container.front();
+         Container.pop();
+         /**************************************************************/
+
+         /*** Step [3] : Process the popped node if it's not seen ***/
+         if(seen[Node_to_be_removed]!=true){
+            // mark it as seen
+            seen[Node_to_be_removed]=true;
+            // use the popped node in your functionality, here we will just print it..
+            cout<<Node_to_be_removed<<endl;
+         }
+         /************************************************************/
+      
+         /*** Step [4] : Add it's unseen childs into the queue ***/
+         for(auto connected_vertex:this->AdjacencyList[Node_to_be_removed]){
+            // connected_vertex is one of childs of the popped vertex, now check if it's not seen before
+            if(seen[connected_vertex]!=true){
+               Container.push(connected_vertex);
+            }
+         }
+         /*********************************************************/
+      }
+   }
+   /*******************************************************************************************/
 };
 
 
 
+
 int main(){
-   Graph G1(4);
-   G1.add_Edge(0,1);
-   G1.add_Edge(0,2);
-   G1.add_Edge(1,2);
-   G1.add_Edge(2,3);
-   G1.print_Ajdacency_List();
+   Graph G1(8);
+   G1.add_Edge("A","B");
+   G1.add_Edge("A","C");
+   G1.add_Edge("A","D");
+   G1.add_Edge("A","E");
+
+   G1.add_Edge("B","G");
+   G1.add_Edge("B","C");
+
+   G1.add_Edge("C","D");
+
+   G1.add_Edge("D","E");
+   G1.add_Edge("D","H");
+
+   G1.add_Edge("E","F");
+
+   G1.add_Edge("F","G");
+   G1.add_Edge("F","H");
+
+
+   G1.BFS("A");
    return 0;
 }
