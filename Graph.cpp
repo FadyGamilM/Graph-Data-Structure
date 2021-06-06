dcd66228cefec1a73655ec320650286ab5e8b572
fady gamil mahrous masoud <1700961@eng.asu.edu.eg>
2021-06-06 18:44:55 +0200

    Adjacency List Representation Of Un-directed & Un-weighted Graph

--- Graph.cpp
+++ Graph.cpp
@@ -21,17 +21,35 @@ public:
    /************************************/
 
    /**************  Add Edge Function *********************/
-   void add_Edge(int first_vertix, int second_vertix ){
+   void add_Edge(int first_vertex, int second_vertex ){
       // Construct/Link the edge between first and second vertix by adding the second_vertix in the AdjacencyList of first_vertix
-      AdjacencyList[first_vertix].push_back(second_vertix);
+      AdjacencyList[first_vertex].push_back(second_vertex);
       // Construct/Link the edge between second and first vertix by adding the first_vertix in the AdjacencyList of second_vertix
-      AdjacencyList[second_vertix].push_back(first_vertix);
+      AdjacencyList[second_vertex].push_back(first_vertex);
    }  
    /*********************************************************/
+
+   /**************  Iterate through all vertices *********************/
+   void print_Ajdacency_List(){
+      for(auto i=0;i<this->Vertices;i++){
+         cout<<"Vertex "<< i << " --> ";
+         for(auto connected_vertex:this->AdjacencyList[i]){
+            cout<<connected_vertex <<", ";
+         } 
+         cout<<endl;
+      }
+   }
+   /******************************************************************/
 };
 
 
 
 int main(){
+   Graph G1(4);
+   G1.add_Edge(0,1);
+   G1.add_Edge(0,2);
+   G1.add_Edge(1,2);
+   G1.add_Edge(2,3);
+   G1.print_Ajdacency_List();
    return 0;
 }
